f68c86a7c6a06a40981f4c933d6edd88e8f069f8
izzijones <izzijones@me.com>
2019-09-03 00:10:54 -0500

    refactored table content to use composition

--- src/components/MenuTabs.js
+++ src/components/MenuTabs.js
@@ -4,59 +4,23 @@ import { Menu, Container} from 'semantic-ui-react'
 export default class MenuTabs extends Component {
     state = { activeItem: 'Medical History' };
 
-    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
     render() {
-        const { activeItem } = this.state
+        const { activeItem } = this.state;
+        const tabNames = ["HPI", "Medical History", "Surgical History", "Medications", "Allergies", "Family History",
+            "Social History", "Review of Systems", "Physical Exam"];
+
+        const tabMenuItems = tabNames.map((name) =>
+            <Menu.Item
+            name={name}
+            active={activeItem === name}
+            onClick={this.handleItemClick}/>);
 
         return (
             <Menu tabular>
                 <Container>
-                    <Menu.Item
-                        name='HPI'
-                        active={activeItem === 'HPI'}
-                        onClick={this.handleItemClick}
-                    />
-                    <Menu.Item
-                        name='Medical History'
-                        active={activeItem === 'Medical History'}
-                        onClick={this.handleItemClick}
-                    />
-                    <Menu.Item
-                        name='surgical history'
-                        active={activeItem === 'surgical history'}
-                        onClick={this.handleItemClick}
-                    />
-                    <Menu.Item
-                        name='Medication'
-                        active={activeItem === 'Medication'}
-                        onClick={this.handleItemClick}
-                    />
-                    <Menu.Item
-                        name='Allergies'
-                        active={activeItem === 'Allergies'}
-                        onClick={this.handleItemClick}
-                    />
-                    <Menu.Item
-                        name='Family History'
-                        active={activeItem === 'Family History'}
-                        onClick={this.handleItemClick}
-                    />
-                    <Menu.Item
-                        name='Social History'
-                        active={activeItem === 'Social History'}
-                        onClick={this.handleItemClick}
-                    />
-                    <Menu.Item
-                        name='Review of Systems'
-                        active={activeItem === 'Review of Systems'}
-                        onClick={this.handleItemClick}
-                    />
-                    <Menu.Item
-                        name='Physical Exam'
-                        active={activeItem === 'Physical Exam'}
-                        onClick={this.handleItemClick}
-                    />
+                    {tabMenuItems}
                 </Container>
             </Menu>
         )
