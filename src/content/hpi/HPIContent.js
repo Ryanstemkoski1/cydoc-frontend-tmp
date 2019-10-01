0f467972519b154f33feffbd60ea0c92ec22441d
izzijones <izzijones@me.com>
2019-10-01 14:24:43 -0500

    Added content to HPI content

--- src/content/hpi/HPIContent.js
+++ src/content/hpi/HPIContent.js
@@ -1,11 +1,68 @@
-import React, {Component, Fragment} from 'react';
+import React, {Component} from 'react';
+// import './App.css';
+import ButtonItem from "./knowledgegraph1/src/ButtonItem"
+import diseaseData from "./knowledgegraph1/src/Diseases";
+import PositiveDiseases from "./knowledgegraph1/src/PositiveDiseases";
+import {Grid} from "semantic-ui-react";
 
-//Component that manages the content for the HPI section.
-export default class HPIContent extends Component{
-    render() {
-        return (
-            <Fragment>
-            {/*Christine, the components you have so far should be added here*/}
-            </Fragment>);
+class HPIContent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            diseaseArray: diseaseData,
+            diseases_positive: []
+        };
+        this.handler = this.handler.bind(this)
     }
-};
+
+    handler(value, id) {
+        let new_array = this.state.diseases_positive;
+        if (id === 1) {
+            new_array = new_array.concat(value)
+        }
+        else if (id===-1) {
+            new_array.splice(new_array.indexOf(value), 1)
+        }
+        this.setState({diseases_positive: new_array})
+    }
+
+    render() {
+        const diseaseComponents = this.createDiseaseComponents();
+        const positiveDiseases = this.createPositiveDiseases();
+        return (
+                <Grid columns={1}>
+                    <Grid.Column>
+                        <Grid.Row>
+                            <div> {positiveDiseases} </div>
+                        </Grid.Row>
+                            {diseaseComponents}
+                    </Grid.Column>
+                </Grid>
+        )
+    }
+
+    createPositiveDiseases() {
+        return this.state.diseases_positive.map(disease =>
+            <PositiveDiseases
+                key={disease}
+                name={disease}
+            />
+        );
+    }
+
+    createDiseaseComponents() {
+        return this.state.diseaseArray.map(item =>
+            <Grid.Row>
+                <ButtonItem
+                    key={item.id}
+                    disease_id={item.id}
+                    name={item.name}
+                    diseases_list={item.diseases}
+                    handler={this.handler}
+                />
+            </Grid.Row>
+        );
+    }
+}
+
+export default HPIContent;
