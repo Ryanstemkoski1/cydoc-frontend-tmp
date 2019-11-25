224008aafd4e6065cf3b869d9be0c40c2674dcd8
AskmienFoodoe <bowen.jiang@duke.edu>
2019-11-25 03:06:25 -0500

    Add static data for Review of Systems Page

--- src/js/content/reviewofsystems/ReviewOfSystemsContent.js
+++ src/js/content/reviewofsystems/ReviewOfSystemsContent.js
@@ -1,29 +1,31 @@
-import React, {Component, Fragment} from 'react';
-import {Grid, Button, Checkbox} from 'semantic-ui-react'
+import React, { Component, Fragment } from 'react';
+import { Grid, Button, Checkbox } from 'semantic-ui-react'
 
 import ReviewOfSystemsCategory from './ReviewOfSystemsCategory'
 
 //Component that manages the content for the Review of Systems section of the note
-export default class ReviewOfSystemsContent extends Component{
+export default class ReviewOfSystemsContent extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
             test: {
-                "category1": ["thing1","quantity2","conceptuality3"],
-                "category2": ["thing1","thing2","thing3"],
-                "category3": ["thing1","thing2","thing3"],
-                "category4": ["thing1","thing2","thing3"],
-                "category5": ["thing1","thing2","thing3", "Bleeding Gums"],
-                "category6": ["thing1","Abdominal Pain","JugemuJugemuGokonosurikire"],
-                "category7": ["thing1","thing2","thing3"],
-                "category8": ["thing1","thing2","thing3"],
-                "category9": ["thing1","thing2","thing3"],
-                "category10": ["thing1","thing2","thing3"],
-                "category11": ["thing1","thing2","thing3"],
-                "category12": ["thing1","thing2","thing3","thing5","thing6","thing4"],
-                "category13": ["thing1","thing2","thing3"],
-                "category14": ["thing1","thing2","thing3"]
+                "General": ["Δ weight", "Fatigue", "Weakness", "Fevers", "Chills", "Night sweats"],
+                "Eyes": ["Glasses", "Contacts", "Blurriness"],
+                "Ears": ["Δ hearing", "Hearing loss", "Tinnitus", "Earache", "Discharge"],
+                "Nose": ["Colds", "Stuffiness", "Sneezing", "Itching", "Allergy", "Nosebleed"],
+                "Mouth": ["Δ teeth", "Bleeding gums", "Sore throat", "Hoarseness"],
+                "Throat": ["Lumps", "Swelling", "Pain", "Stiffness"],
+                "Respiratory": ["Dyspnea", "Dyspnea on exertion", "Nocturnal dyspnea", "Orthopnea", "Wheezing", "Cough", "Asthma", "Bronchitis", "Emphysema", "Pneumonia", "Tuberculosis"],
+                "Cardiovascular/Hematological": ["Chest pain", "Palpitations", "High BP", "Low BP", "Murmurs", "Edema", "Varicose veins", "Claudication", "Easy bruising", "Easy bleeding", "Anemia", "Transfusions"],
+                "Gastrointestinal": ["Δ appetite", "Nausea", "Vomiting", "Abdominal pain", "Dysphagia", "Heartburn", "Bloating", "Diarrhea", "Constipation", "Hematemesis", "Hemorrhoids", "Melena", "Hematochezia"],
+                "Genitourinary": ["Urinary tract infection", "Δ stream", "Frequency", "Hesitancy", "Urgency", "Polyuria", "Hematuria", "Nocturia", "Incontinence", "Stones"],
+                "Genital/Sexual/Gynecological": ["Discharge", "Sores", "Itching", "STD", "Contraception", "Hernias", "Testicular/vaginal pain", "Testicular mass", "Breast pain", "Breast masses", "Breast lumps", "Discharge", "Period irregularities", "Pregnancy complications"],
+                "Musculoskeletal": ["Osteoarthritis", "Rheumatoid arthritis", "Joint stiffness", "Joint pain", "Joint swelling", "Muscle cramps", "Muscle weakness", "Muscle pain"],
+                "Skin/Hair/Nails": ["Rashes", "Itching", "Dryness", "Δ hair", "Δ nails", "Sores", "Lumps", "Moles"],
+                "Endocrine": ["Heat intolerance", "Cold intolerance","Excessive sweating","Polydipsia","Polyphagia","Hyperthyroidism","Hypothyroidism","Diabetes","Skin color change","Excess hair growth"],
+                "Neuro": ["Headache","Δ vision","Double vision","Fainting/blackouts","Seizures","Paralysis","Numbness","Tingling","Loss of sensation","Vertigo/dizziness","Tremor","Difficulty walking","Δ coordination","Confusion","Memory loss"],
+                "Psych": ["Anxiety", "Depression", "Suicide attempts"]
             }
         }
     }
@@ -31,11 +33,11 @@ export default class ReviewOfSystemsContent extends Component{
 
     generateList = (systemsCategories) => {
         return Object.keys(systemsCategories).map(
-            (label) => 
+            (label) =>
                 <Grid.Column>
-                    <ReviewOfSystemsCategory category={label} options={systemsCategories[label]}/>
+                    <ReviewOfSystemsCategory category={label} options={systemsCategories[label]} />
                 </Grid.Column>
-            
+
         )
     }
 
