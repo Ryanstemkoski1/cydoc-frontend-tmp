09fe06748ae100dc13242875924628e190f27b99
Regan Baum <regan.baum@duke.edu>
2020-04-20 17:50:33 -0400

    fixed missing file

--- src/js/content/medications/MedicationsContent.js
+++ src/js/content/medications/MedicationsContent.js
@@ -3,7 +3,6 @@ import TableContent from '../../components/TableContent';
 import { medications } from '../../constants/States';
 import HPIContext from '../../contexts/HPIContext';
 import drug_names from '../../constants/drugNames';
-import '../../../css/content/medicationsContent.css';
 
 //Component that manages content for the Medications page
 export default class MedicationsContent extends Component {
