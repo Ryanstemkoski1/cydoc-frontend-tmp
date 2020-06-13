0133ab04988a5f891874c8fb98030bcb8ee91ecc
Amy <leiamy12@gmail.com>
2020-06-13 18:55:37 -0400

    further distinguish survey from plan

--- src/pages/EditNote/content/discussionplan/DiscussionPlan.js
+++ src/pages/EditNote/content/discussionplan/DiscussionPlan.js
@@ -135,80 +135,81 @@ class plan extends Component {
                     <ProcedureForm index={current} mobile={mobile}/>
                     <ReferralForm index={current} mobile={mobile}/>
                 </React.Fragment>)}
-                {/* <Divider horizontal as='h2' className='plan-survey-divider'>Help Improve Cydoc</Divider> */}
-                <Header as='h4' attached='top'> Help Improve Cydoc </Header>
-                <Segment attached piled>
-                    <Grid stackable columns={2}>
-                        <Grid.Row>
-                            <Grid.Column width={7}>
-                                <h4> How sick is the patient? </h4>
-                            </Grid.Column>
-                            <Grid.Column floated='right'>
-                                <label> Healthy </label>
-                                <input 
-                                    type='range' 
-                                    min={0} 
-                                    max={10} 
-                                    step={1} 
-                                    value={plan['survey']['sickness']}
-                                    onChange={(e) => this.handleInputChange('sickness', parseInt(e.target.value))} 
-                                    />
-                                <label> Sick </label>
-                                <Label circular>{plan['survey']['sickness']}</Label>
-                            </Grid.Column>
-                        </Grid.Row>
-                        <Grid.Row>
-                            <Grid.Column width={7}>
-                                <h4> Will the patient be sent to the emergency department? </h4>
-                            </Grid.Column>
-                            <Grid.Column floated='right'>
-                                <button 
-                                    className="button_yesno" 
-                                    style={{backgroundColor: plan['survey']['emergency'] === 'Yes' ? this.active_color : this.nonactive_color}} 
-                                    onClick={() => this.handleOnClick('emergency', 'Yes')}> 
-                                    Yes 
-                                </button> 
-                                <button 
-                                    className="button_yesno" 
-                                    style={{backgroundColor: plan['survey']['emergency'] === 'No' ? this.active_color : this.nonactive_color}} 
-                                    onClick={() => this.handleOnClick('emergency', 'No')}> 
-                                    No 
-                                </button> 
-                                <button 
-                                    className="button_yesno" 
-                                    style={{backgroundColor: plan['survey']['emergency'] === 'Uncertain' ? this.active_color : this.nonactive_color}} 
-                                    onClick={() => this.handleOnClick('emergency', 'Uncertain')}> 
-                                    Uncertain 
-                                </button> 
-                            </Grid.Column>
-                        </Grid.Row>
-                        <Grid.Row>
-                            <Grid.Column width={7}>
-                                <h4> Will the patient be admitted to the hospital? </h4>
-                            </Grid.Column>
-                            <Grid.Column floated='right'>
-                                <button 
-                                    className="button_yesno" 
-                                    style={{backgroundColor: plan['survey']['admit_to_hospital'] === 'Yes' ? this.active_color : this.nonactive_color}} 
-                                    onClick={() => this.handleOnClick('admit_to_hospital', 'Yes')}> 
-                                    Yes 
-                                </button> 
-                                <button 
-                                    className="button_yesno" 
-                                    style={{backgroundColor: plan['survey']['admit_to_hospital'] === 'No' ? this.active_color : this.nonactive_color}} 
-                                    onClick={() => this.handleOnClick('admit_to_hospital', 'No')}> 
-                                    No 
-                                </button> 
-                                <button 
-                                    className="button_yesno" 
-                                    style={{backgroundColor: plan['survey']['admit_to_hospital'] === 'Uncertain' ? this.active_color : this.nonactive_color}} 
-                                    onClick={() => this.handleOnClick('admit_to_hospital', 'Uncertain')}> 
-                                    Uncertain 
-                                </button> 
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
-                </Segment>
+                <div className='plan-survey'>
+                    <Header as='h4' attached='top'> Help Improve Cydoc </Header>
+                    <Segment attached>
+                        <Grid stackable columns={2}>
+                            <Grid.Row>
+                                <Grid.Column width={7}>
+                                    <h4> How sick is the patient? </h4>
+                                </Grid.Column>
+                                <Grid.Column floated='right'>
+                                    <label> Healthy </label>
+                                    <input 
+                                        type='range' 
+                                        min={0} 
+                                        max={10} 
+                                        step={1} 
+                                        value={plan['survey']['sickness']}
+                                        onChange={(e) => this.handleInputChange('sickness', parseInt(e.target.value))} 
+                                        />
+                                    <label> Sick </label>
+                                    <Label circular>{plan['survey']['sickness']}</Label>
+                                </Grid.Column>
+                            </Grid.Row>
+                            <Grid.Row>
+                                <Grid.Column width={7}>
+                                    <h4> Will the patient be sent to the emergency department? </h4>
+                                </Grid.Column>
+                                <Grid.Column floated='right'>
+                                    <button 
+                                        className="button_yesno" 
+                                        style={{backgroundColor: plan['survey']['emergency'] === 'Yes' ? this.active_color : this.nonactive_color}} 
+                                        onClick={() => this.handleOnClick('emergency', 'Yes')}> 
+                                        Yes 
+                                    </button> 
+                                    <button 
+                                        className="button_yesno" 
+                                        style={{backgroundColor: plan['survey']['emergency'] === 'No' ? this.active_color : this.nonactive_color}} 
+                                        onClick={() => this.handleOnClick('emergency', 'No')}> 
+                                        No 
+                                    </button> 
+                                    <button 
+                                        className="button_yesno" 
+                                        style={{backgroundColor: plan['survey']['emergency'] === 'Uncertain' ? this.active_color : this.nonactive_color}} 
+                                        onClick={() => this.handleOnClick('emergency', 'Uncertain')}> 
+                                        Uncertain 
+                                    </button> 
+                                </Grid.Column>
+                            </Grid.Row>
+                            <Grid.Row>
+                                <Grid.Column width={7}>
+                                    <h4> Will the patient be admitted to the hospital? </h4>
+                                </Grid.Column>
+                                <Grid.Column floated='right'>
+                                    <button 
+                                        className="button_yesno" 
+                                        style={{backgroundColor: plan['survey']['admit_to_hospital'] === 'Yes' ? this.active_color : this.nonactive_color}} 
+                                        onClick={() => this.handleOnClick('admit_to_hospital', 'Yes')}> 
+                                        Yes 
+                                    </button> 
+                                    <button 
+                                        className="button_yesno" 
+                                        style={{backgroundColor: plan['survey']['admit_to_hospital'] === 'No' ? this.active_color : this.nonactive_color}} 
+                                        onClick={() => this.handleOnClick('admit_to_hospital', 'No')}> 
+                                        No 
+                                    </button> 
+                                    <button 
+                                        className="button_yesno" 
+                                        style={{backgroundColor: plan['survey']['admit_to_hospital'] === 'Uncertain' ? this.active_color : this.nonactive_color}} 
+                                        onClick={() => this.handleOnClick('admit_to_hospital', 'Uncertain')}> 
+                                        Uncertain 
+                                    </button> 
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </Segment>
+                </div>
             </div>
         )
     }
