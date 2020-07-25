0ee09c52aaf02544f316906a8423abfd67891724
AskmienFoodoe <bowen.jiang@duke.edu>
2020-07-25 12:56:48 -0400

    Add ROS tests and code coverage reports

--- src/tests/pages/EditNote/content/reviewofsystems/ReviewOfSystemsContent.test.js
+++ src/tests/pages/EditNote/content/reviewofsystems/ReviewOfSystemsContent.test.js
@@ -1,27 +1,28 @@
 import React from 'react';
-import Enzyme, { shallow, mount } from 'enzyme';
+import Enzyme, { shallow, mount, render } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { HPIStore } from 'contexts/HPIContext';
 import ReviewOfSystemsContent from '../../../../../pages/EditNote/content/reviewofsystems/ReviewOfSystemsContent';
 import ReviewOfSystemsCategory from 'pages/EditNote/content/reviewofsystems/ReviewOfSystemsCategory';
 import { sections } from 'constants/review-of-systems-constants';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-const setup = (context) => {
+//TODO: Refactor components to have independent state from context
+const mountWithContext = () => {
     return mount(
-        <AuthContext.Provider value={context}>
-            <EditProfile />
-        </AuthContext.Provider>
+        <HPIStore>
+            <ReviewOfSystemsContent />
+        </HPIStore>
     );
 }
 
 test('renders without crashing', () => {
-    const wrapper = shallow(<ReviewOfSystemsContent/>);
+    const wrapper = mountWithContext();
     expect(wrapper).toBeTruthy();
 });
-/*
+
 test('renders correct number of categories', () => {
-    const wrapper = shallow(<ReviewOfSystemsContent/>).find(<ReviewOfSystemsCategory/>);
+    const wrapper = mountWithContext().find(ReviewOfSystemsCategory);
     expect(wrapper).toHaveLength(Object.keys(sections).length);
 })
-*/
