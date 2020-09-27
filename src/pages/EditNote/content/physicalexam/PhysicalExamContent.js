981a5565e3acfc6de0a2734c9805a43b4d0419e3
Paul Rhee <paulrhee502@gmail.com>
2020-09-27 13:58:11 -0400

    Implemented headers with drop downs for mobile view of physical exam

--- src/pages/EditNote/content/physicalexam/PhysicalExamContent.js
+++ src/pages/EditNote/content/physicalexam/PhysicalExamContent.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from 'react';
-import { Form, Grid, Input, Button } from "semantic-ui-react";
+import { Accordion, Divider, Form, Grid, Header, Icon, Input, Button } from "semantic-ui-react";
 import PhysicalExamGroup from './PhysicalExamGroup';
 import constants from 'constants/physical-exam-constants.json'
 import HPIContext from 'contexts/HPIContext.js';
 import Segment from "semantic-ui-react/dist/commonjs/elements/Segment";
+import {PHYSICAL_EXAM_MOBILE_BP} from "constants/breakpoints.js";
 
 //Component that manages content for the Physical Exam tab
 export default class PhysicalExamContent extends React.Component {
@@ -12,6 +13,40 @@ export default class PhysicalExamContent extends React.Component {
 
     constructor(props) {
         super(props)
+        let isActiveIndex = []
+        // Initializing boolean array of false for each section in the physical exam content
+        for (let i  = 0; i < constants.sections.length + 1; i++){
+            isActiveIndex.push(false)
+        }
+        this.state = {
+            windowWidth: 0,
+            windowHeight: 0,
+            isActiveIndex
+        }
+        this.updateDimensions = this.updateDimensions.bind(this);
+    }
+
+    componentDidMount() {
+        this.updateDimensions();
+        window.addEventListener("resize", this.updateDimensions);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateDimensions);
+    }
+
+    updateDimensions() {
+        let windowWidth = typeof window !== "undefined" ? window.innerWidth : 0;
+        let windowHeight = typeof window !== "undefined" ? window.innerHeight : 0;
+ 
+        this.setState({ windowWidth, windowHeight });
+    }
+
+    handleClick = (e, data) => {
+        const { active, index } = data
+        let newIsActiveState = this.state.isActiveIndex
+        newIsActiveState[index] = !active
+        this.setState({activeIndex: newIsActiveState})
     }
 
     handleChange = (category, name, value) => {
@@ -33,60 +68,147 @@ export default class PhysicalExamContent extends React.Component {
         )
     }
 
-    renderGroups = (groups) => {
-        return groups.map((section) =>
-            <Segment>
+    renderSegments = (groups) => {
+        const segments = [
+        <Segment>
+            <Form>
+                <Grid stackable columns="equal">
+                    <Grid.Column>
+                        <h5>Blood Pressure (mmHg)</h5>
+                        <Form.Field>
+                            {this.generateNumericInput("Vitals", "Systolic Blood Pressure", "systolic", "right")}
+                        </Form.Field>
+                        <Form.Field>
+                            {this.generateNumericInput("Vitals", "Diastolic Blood Pressure", "diastolic", "right")}
+                        </Form.Field>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <h5>Heart Rate</h5>
+                        <Form.Field>
+                            {this.generateNumericInput("Vitals", "Heart Rate", "bpm", "right")}
+                        </Form.Field>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <h5>RR</h5>
+                        <Form.Field>
+                            {this.generateNumericInput("Vitals", "RR")}
+                        </Form.Field>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <h5>Temperature</h5>
+                        <Form.Field>
+                            {this.generateNumericInput("Vitals", "Temperature", "℃", "right")}
+                        </Form.Field>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <h5>Oxygen Saturation</h5>
+                        <Form.Field>
+                            {this.generateNumericInput("Vitals", "Oxygen Saturation")}
+                        </Form.Field>
+                    </Grid.Column>
+                </Grid>
+            </Form>
+        </Segment>]
+        groups.forEach((section) =>
+            segments.push(<Segment>
                 <PhysicalExamGroup name={section.name} rows={section.rows} />
-            </Segment>
+            </Segment>)
         )
+        return segments
+    }
+    
+    renderMobileSections = (groups) => {
+        const { isActiveIndex } = this.state
+        const segments = [
+            {
+                key: 'Vitals',
+                title: {
+                    content: "Vitals",
+                    icon: 'dropdown'
+                },
+                active: isActiveIndex[0],
+                onTitleClick: this.handleClick,
+                content: {
+                    content: (
+                        <>
+                        <Header as="h2">Vitals</Header>
+                        <Divider />
+                        <Form>
+                            <Grid stackable columns="equal">
+                                <Grid.Column>
+                                    <h5>Blood Pressure (mmHg)</h5>
+                                    <Form.Field>
+                                        {this.generateNumericInput("Vitals", "Systolic Blood Pressure", "systolic", "right")}
+                                    </Form.Field>
+                                    <Form.Field>
+                                        {this.generateNumericInput("Vitals", "Diastolic Blood Pressure", "diastolic", "right")}
+                                    </Form.Field>
+                                </Grid.Column>
+                                <Grid.Column>
+                                    <h5>Heart Rate</h5>
+                                    <Form.Field>
+                                        {this.generateNumericInput("Vitals", "Heart Rate", "bpm", "right")}
+                                    </Form.Field>
+                                </Grid.Column>
+                                <Grid.Column>
+                                    <h5>RR</h5>
+                                    <Form.Field>
+                                        {this.generateNumericInput("Vitals", "RR")}
+                                    </Form.Field>
+                                </Grid.Column>
+                                <Grid.Column>
+                                    <h5>Temperature</h5>
+                                    <Form.Field>
+                                        {this.generateNumericInput("Vitals", "Temperature", "℃", "right")}
+                                    </Form.Field>
+                                </Grid.Column>
+                                <Grid.Column>
+                                    <h5>Oxygen Saturation</h5>
+                                    <Form.Field>
+                                        {this.generateNumericInput("Vitals", "Oxygen Saturation")}
+                                    </Form.Field>
+                                </Grid.Column>
+                            </Grid>
+                        </Form>
+                        </>
+                    )
+                }
+            }
+        ]
+        for (let i = 1; i < groups.length + 1; i++) {
+            segments.push(
+                {
+                    key: groups[i - 1].name,
+                    title: {
+                        content: groups[i - 1].name,
+                        icon: 'dropdown'
+                    },
+                    active: isActiveIndex[i],
+                    onTitleClick: this.handleClick,
+                    content: {
+                        content: (
+                            <PhysicalExamGroup name={groups[i - 1].name} rows={groups[i - 1].rows} />
+                        )
+                    }
+                }
+            )
+        }
+        return segments
     }
 
     render() {
+        const windowWidth = this.state.windowWidth
         return (
             <>
-            <Segment>
-                <Form>
-                    <Grid stackable columns="equal">
-                        <Grid.Column>
-                            <h5>Blood Pressure (mmHg)</h5>
-                            <Form.Field>
-                                {this.generateNumericInput("Vitals", "Systolic Blood Pressure", "systolic", "right")}
-                            </Form.Field>
-                            <Form.Field>
-                                {this.generateNumericInput("Vitals", "Diastolic Blood Pressure", "diastolic", "right")}
-                            </Form.Field>
-                        </Grid.Column>
-                        <Grid.Column>
-                            <h5>Heart Rate</h5>
-                            <Form.Field>
-                                {this.generateNumericInput("Vitals", "Heart Rate", "bpm", "right")}
-                            </Form.Field>
-                        </Grid.Column>
-                        <Grid.Column>
-                            <h5>RR</h5>
-                            <Form.Field>
-                                {this.generateNumericInput("Vitals", "RR")}
-                            </Form.Field>
-                        </Grid.Column>
-                        <Grid.Column>
-                            <h5>Temperature</h5>
-                            <Form.Field>
-                                {this.generateNumericInput("Vitals", "Temperature", "℃", "right")}
-                            </Form.Field>
-                        </Grid.Column>
-                        <Grid.Column>
-                            <h5>Oxygen Saturation</h5>
-                            <Form.Field>
-                                {this.generateNumericInput("Vitals", "Oxygen Saturation")}
-                            </Form.Field>
-                        </Grid.Column>
-                    </Grid>
-                </Form>
-            </Segment>
-        {this.renderGroups(constants.sections)}
-        </>
-
-    )
+                {windowWidth != 0 && windowWidth < PHYSICAL_EXAM_MOBILE_BP ?
+                    <Accordion styled fluid panels = {this.renderMobileSections(constants.sections)}/>
+                :
+                    <>
+                {this.renderSegments(constants.sections)}
+                    </>
+                }
+            </>
+        )
     }
 
 }
