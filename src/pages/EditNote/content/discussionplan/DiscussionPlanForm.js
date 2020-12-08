daa62815633d48782b977587648391e0e5df3f37
Jamie Luong <jamieluong03@gmail.com>
2020-12-07 20:21:57 -0800

    removed discussion plan dropdown changes

--- src/pages/EditNote/content/discussionplan/DiscussionPlanForm.js
+++ src/pages/EditNote/content/discussionplan/DiscussionPlanForm.js
@@ -10,12 +10,10 @@ import {
 import AddRowButton from 'components/tools/AddRowButton';
 import { FORM_DEFAULTS } from './DiscussionPlanDefaults';
 import HPIContext from 'contexts/HPIContext.js'
-import diseases from 'constants/differential_diagnoses';
+import diseases from 'constants/diseases';
 import medications from 'constants/drugNames';
 import procedures from 'constants/procedures';
 import constants from 'constants/registration-constants.json';
-import _ from 'lodash';
-import './discussionPlan.css';
 const specialtyOptions = constants.specialties.map((specialty) => ({ key: specialty, text: specialty, value: specialty }));
 
 const TYPE_TO_OPTIONS = {
@@ -34,10 +32,8 @@ export default class DiscussionPlanForm extends Component{
             active: new Set(),
             mainOptions: TYPE_TO_OPTIONS[this.props.type],
             whenOptions: this.generateOptions(['today', 'this week', 'this month', 'this year',]),
-            disease: ''
         }
         this.handleOnChange = this.handleOnChange.bind(this);
-        this.grabValue = this.grabValue.bind(this);
     }
     
     generateOptions = (values) => {
@@ -54,7 +50,6 @@ export default class DiscussionPlanForm extends Component{
     }
 
     handleAddOption = (e, { optiontype, value }) => {
-        console.log('ADDOPTION', value)
         this.setState((prevState) => ({
             [optiontype]: [
                 { value, key: value, text: value },
@@ -64,7 +59,6 @@ export default class DiscussionPlanForm extends Component{
     }
 
     handleOnChange = (e, { index, name, value }) => {
-        console.log('index', index, 'name', name, 'value', value)
         if (name === 'diagnosis' && !this.state.active.has(index)) {
             this.toggleAccordion(index);
         }
@@ -90,10 +84,6 @@ export default class DiscussionPlanForm extends Component{
         this.setState({ active });
     }
 
-    componentDidMount() {
-        this.autocomplete(document.getElementById("myInput"), TYPE_TO_OPTIONS['differential_diagnosis'])
-    }
-
     componentDidUpdate(prevProps) {
         if (prevProps.index !== this.props.index) {
             this.setState({ expandPanels: false });
@@ -106,141 +96,27 @@ export default class DiscussionPlanForm extends Component{
             .replace(/\w\S*/g, text => text === 'and' ? text : text.charAt(0).toUpperCase() + text.substr(1));
     }
 
-    autocomplete(inp, obj) {
-        /*the autocomplete function takes two arguments,
-        the text field element and an array of possible autocompleted values:*/
-        
-        /* removes the undefined values in the obj */
-        var arr = _.compact(_.map(obj, o => o.text));
-
-        var currentFocus;
-        const myInput = global.document.getElementById('myInput');
-
-        /*execute a function when someone writes in the text field:*/
-        inp.addEventListener("input", function(e) {
-            var a, b, val = this.value;
-            /*close any already open lists of autocompleted values*/
-            closeAllLists();
-            if (!val) { return false;}
-            currentFocus = -1;
-            /*create a DIV element that will contain the items (values):*/
-            a = document.createElement("DIV");
-            a.setAttribute("id", this.id + "autocomplete-list");
-            a.setAttribute("class", "autocomplete-items");
-            /*append the DIV element as a child of the autocomplete container:*/
-            this.parentNode.appendChild(a);
-            for (let i=0; i < arr.length; i++) {
-                /*check if the item starts with the same letters as the text field value:*/
-                if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-                  /*create a DIV element for each matching element:*/
-                    b = document.createElement("DIV");
-                    /*make the matching letters bold:*/
-                    b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-                    b.innerHTML += arr[i].substr(val.length);
-                    /*insert a input field that will hold the current array item's value:*/
-                    b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-
-                    /*execute a function when someone clicks on the item value (DIV element):*/
-                    b.addEventListener("click", function(e) {
-                    /*insert the value for the autocomplete text field:*/
-                        inp.value = this.getElementsByTagName("input")[0].value;              
-                        myInput.setAttribute('value', inp.value);
-                        myInput.innerHTML = inp.value;
-                        /*close the list of autocompleted values,
-                        (or any other open lists of autocompleted values:*/
-                        closeAllLists();
-                    });
-                    a.appendChild(b);
-                }
-            }
-        });
-        /*execute a function presses a key on the keyboard:*/
-        inp.addEventListener("keydown", function(e) {
-            var x = document.getElementById(this.id + "autocomplete-list");
-            if (x) x = x.getElementsByTagName("div");
-            if (e.keyCode == 40) {
-              /*If the arrow DOWN key is pressed,
-              increase the currentFocus variable:*/
-              currentFocus++;
-              /*and and make the current item more visible:*/
-              addActive(x);
-            } else if (e.keyCode == 38) { //up
-              /*If the arrow UP key is pressed,
-              decrease the currentFocus variable:*/
-              currentFocus--;
-              /*and and make the current item more visible:*/
-              addActive(x);
-            } else if (e.keyCode == 13) {
-              /*If the ENTER key is pressed, prevent the form from being submitted,*/
-              e.preventDefault();
-              if (currentFocus > -1) {
-                /*and simulate a click on the "active" item:*/
-                if (x) x[currentFocus].click();
-              }
-            }
-        });
-        function addActive(x) {
-          /*a function to classify an item as "active":*/
-          if (!x) return false;
-          /*start by removing the "active" class on all items:*/
-          removeActive(x);
-          if (currentFocus >= x.length) currentFocus = 0;
-          if (currentFocus < 0) currentFocus = (x.length - 1);
-          /*add class "autocomplete-active":*/
-          x[currentFocus].classList.add("autocomplete-active");
-        }
-        function removeActive(x) {
-          /*a function to remove the "active" class from all autocomplete items:*/
-          for (var i = 0; i < x.length; i++) {
-            x[i].classList.remove("autocomplete-active");
-          }
-        }
-        function closeAllLists(elmnt) {
-          /*close all autocomplete lists in the document,
-          except the one passed as an argument:*/
-          var x = document.getElementsByClassName("autocomplete-items");
-          for (var i = 0; i < x.length; i++) {
-            if (elmnt != x[i] && elmnt != inp) {
-            x[i].parentNode.removeChild(x[i]);
-          }
-        }
-      }
-      /*execute a function when someone clicks in the document:*/
-      document.addEventListener("click", function (e) {
-          closeAllLists(e.target);
-      });
-      }
-    
-    grabValue(e) {
-        console.log('e', e, e.target.value)
-        const target = e.target.value;
-        this.setState({ disease: target});
-        console.log('STATE', this.state.disease)
-        document.getElementById('myInput').value = e.target.value;
-        console.log(document.getElementById('myInput').value)
-        
-    }
-
     makeGridContent = (type, data) => {
         const gridBody = data.map((datum, idx) => {
             switch(type) {
                 case 'differential_diagnosis':
                     return <Grid.Row key={idx}>
                         <Grid.Column width={6}>
-                            <form autocomplete='off' action='/action_page.php'>
-                                <div class='autocomplete'>
-                                    <Input
-                                        id='myInput'
-                                        type='text' 
-                                        placeholder='Diagnosis' 
-                                        name='diagnosis'
-                                        index={idx}
-                                        // value={datum["diagnosis"]}
-                                        options={TYPE_TO_OPTIONS['differential_diagnosis']}
-                                        onChange={this.grabValue}
-                                    />
-                                </div>
-                            </form>
+                            <Dropdown
+                                fluid
+                                search
+                                selection
+                                allowAdditions
+                                icon=''
+                                optiontype='mainOptions'
+                                name='diagnosis'
+                                index={idx}
+                                value={datum['diagnosis']}
+                                options={this.state.mainOptions}
+                                onChange={this.handleOnChange}
+                                onAddItem={this.handleAddOption}
+                                placeholder={'diagnosis'}
+                            />
                         </Grid.Column>
                         <Grid.Column width={10}>
                             <Input
@@ -390,10 +266,8 @@ export default class DiscussionPlanForm extends Component{
         return data.map((datum, idx) => {
             let title;
             let content;
-            if (
-                // type === 'differential_diagnosis'
-                // || 
-                type === 'procedures_and_services'
+            if (type === 'differential_diagnosis'
+                || type === 'procedures_and_services'
                 || type === 'referrals') {
                 title = (
                     <Input 
@@ -418,24 +292,6 @@ export default class DiscussionPlanForm extends Component{
                         />
                     </Input>
                 );
-            } else if (type === 'differential_diagnosis') {
-                title = (
-                    <form autocomplete='off' action='/action_page.php'>
-                        <div class='autocomplete'>
-                            <Input
-                                id='myInput'
-                                type='text' 
-                                placeholder='Diagnosis' 
-                                name='diagnosis'
-                                index={idx}
-                                value={datum['diagnosis']}
-                                options={TYPE_TO_OPTIONS['differential_diagnosis']}
-                                onChange={this.handleOnChange}
-                                onAddItem={this.handleAddOption}
-                            />
-                        </div>
-                    </form>
-                )
             } else {
                 title = (
                     <div className='recipe'>
@@ -610,4 +466,3 @@ export default class DiscussionPlanForm extends Component{
         )
     }
 }
-
