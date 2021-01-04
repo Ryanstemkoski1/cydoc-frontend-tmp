a6ef7aaaad540247043ffbba8260f32de13c3420
aihirsch <aihirsch@yahoo.com>
2021-01-04 13:35:29 -0500

    Update GenerateNote.js
    fix noncompile

--- src/pages/EditNote/content/generatenote/GenerateNote.js
+++ src/pages/EditNote/content/generatenote/GenerateNote.js
@@ -1,6 +1,6 @@
 import  React from 'react';
 import HPIContext from '../../../../contexts/HPIContext';
-import { Button, Segment } from 'semantic-ui-react';
+import { Button, Segment, Icon } from 'semantic-ui-react';
 
 // import all the individual note sections
 import MedicalHistoryNote from './notesections/MedicalHistoryNote';
