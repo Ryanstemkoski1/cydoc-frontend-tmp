01a3a94cd58a1141371e4ea0cdcdae81d563dfa4
cyoon23 <christineyoon@Christines-MacBook-Pro.local>
2021-08-01 23:52:44 -0400

    remove disease form question

--- src/pages/EditNote/content/hpi/knowledgegraph/src/components/CreateResponse.tsx
+++ src/pages/EditNote/content/hpi/knowledgegraph/src/components/CreateResponse.tsx
@@ -156,7 +156,7 @@ class CreateResponse extends React.Component<Props, CreateResponseState> {
                         key={node}
                         isPreview={false}
                         mobile={collapseTabs}
-                        values={this.popResponseChoice()}
+                        values={responseChoice}
                         responseType={responseType}
                         node={node}
                     />
