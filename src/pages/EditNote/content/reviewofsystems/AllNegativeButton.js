7b9d9e8c94bbe5ed24e6bf9197202425a356cdc8
Amy Lei <42757189+amy-lei@users.noreply.github.com>
2021-10-30 10:15:18 -0400

    Implement 'select all negative' button (#169)

--- src/pages/EditNote/content/reviewofsystems/AllNegativeButton.js
+++ src/pages/EditNote/content/reviewofsystems/AllNegativeButton.js
@@ -1,37 +1,41 @@
 import React, { Fragment } from 'react';
 import { Button } from 'semantic-ui-react';
-//import './PhysicalExam.css';
-//import NumericInput from 'react-numeric-input';
+import { YesNoResponse } from 'constants/enums';
 
 //Component that triggers its handleClick function on all children when clicked
-export default class AllNegativeButton extends React.Component {
-    render = () => {
-        return (
-            <Fragment>
-                <Button
-                    toggle
-                    content='Select All No'
-                    active={
-                        //goes through every no buttons and see if they are active
-                        this.props.children.every(
-                            (currentOption) =>
-                                currentOption.props.children[0].props.children
-                                    .props.active
-                        )
-                    }
-                    onClick={() => {
-                        // calls the handleClick method of child toggles and sets to 'no'
-                        React.Children.map(this.props.children, (child) =>
-                            this.props.handleClick(
-                                child.props.children[1].props.children,
-                                'n'
-                            )
-                        );
-                    }}
-                    className={'spaced-buttons'}
-                />
-                {this.props.children}
-            </Fragment>
+const AllNegativeButton = (props) => {
+    // whether every No button is selected
+    const { children, handleClick } = props;
+    const allNegative =
+        !children ||
+        children.every(
+            (currentOption) =>
+                currentOption.props.children[0].props.children.props.active
         );
-    };
-}
+    return (
+        <Fragment>
+            <Button
+                toggle
+                content='Select All Negative'
+                active={allNegative}
+                onClick={() =>
+                    React.Children.map(children, (child) => {
+                        const optionName =
+                            child.props.children?.[1]?.props?.children;
+                        const noButton =
+                            child.props.children?.[0]?.props?.children;
+                        if (allNegative) {
+                            handleClick(optionName, null);
+                        } else if (!noButton?.props?.active) {
+                            handleClick(optionName, YesNoResponse.No);
+                        }
+                    })
+                }
+                className='select-all-negative'
+            />
+            {children}
+        </Fragment>
+    );
+};
+
+export default AllNegativeButton;
