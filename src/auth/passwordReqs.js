c614d1246d9812c837f077f28aa1037cffa60a64
Colin Zhu <33072296+colinz11@users.noreply.github.com>
2021-11-30 15:19:20 -0500

    Forgot password (#184)
    * Fixed forgot password issue
    
    * linting fix
    
    * 16 character reqiurment for password doctors
    
    * linting:
    
    * change password 16 character fixed:
    
    * linting
    
    * moves password reqs to seperate file to generalize more
    
    * linting
    
    * lint:
    
    * linting
    
    * lint
    
    Co-authored-by: Colin Zhu <colinzhu@Colins-MacBook-Pro.local>

---
+++ src/auth/passwordReqs.js
@@ -0,0 +1,32 @@
+const doctorPasswordLength = 16;
+const managerPasswordLength = 25;
+
+export function passwordRequirments(passwordReqs, value, role) {
+    if (role === 'doctor') {
+        return {
+            ...passwordReqs,
+            containsNumber: value.match(/\d+/g) ? true : false,
+            containsUpper: value.toLowerCase() !== value,
+            containsLower: value.toUpperCase() !== value,
+            containsSpecial: value.match(
+                /=+|\++|-+|\^+|\$+|\*+|\.+|\[+|\]+|{+|}+|\(+|\)+|\?+|"+|!+|@+|#+|%+|&+|\/+|\\+|,+|>+|<+|'+|:+|;+|\|+|_+|~+|`+/g
+            )
+                ? true
+                : false,
+            passesMinLength: value.length >= doctorPasswordLength,
+        };
+    } else {
+        return {
+            ...passwordReqs,
+            containsNumber: value.match(/\d+/g) ? true : false,
+            containsUpper: value.toLowerCase() !== value,
+            containsLower: value.toUpperCase() !== value,
+            containsSpecial: value.match(
+                /=+|\++|-+|\^+|\$+|\*+|\.+|\[+|\]+|{+|}+|\(+|\)+|\?+|"+|!+|@+|#+|%+|&+|\/+|\\+|,+|>+|<+|'+|:+|;+|\|+|_+|~+|`+/g
+            )
+                ? true
+                : false,
+            passesMinLength: value.length >= managerPasswordLength,
+        };
+    }
+}
