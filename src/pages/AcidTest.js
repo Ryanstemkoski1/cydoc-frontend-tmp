6231948ec5f038e6dff8169a38a177065d82c36e
Howie Youngdahl <94875209+freshjuicez@users.noreply.github.com>
2023-01-25 10:27:32 -0500

    added callbacks, changed styling
    refined colors, added callbacks to maintain state in parent, added button to calculate results.

--- src/pages/AcidTest.js
+++ src/pages/AcidTest.js
@@ -4,8 +4,34 @@ import Logo from '../assets/cydoc-logo.svg';
 import { useHistory } from "react-router";
 import { Menu } from "semantic-ui-react";
 import { Image } from "semantic-ui-react";
+import { useState } from "react";
 
 const AcidTest = () => {
+  const [pH, setPH] = useState(0);
+  const [HC, setHC] = useState(0);
+  const [PC, setPC] = useState(0);
+  const [nA, setNA] = useState(0);
+  const [Cl, setCl] = useState(0);
+  const [Albumin, setAlbumin] = useState(0);
+
+  const onPhChange = ( number ) => {
+    setPH(number);
+  }
+  const onHCChange = ( number ) => {
+    setHC(number);
+  }
+  const onPCChange = ( number ) => {
+    setPC(number);
+  }
+  const onNaChange = ( number ) => {
+    setNA(number);
+  }
+  const onClChange = ( number ) => {
+    setCl(number);
+  }
+  const onAlbuminChange = ( number ) => {
+    setAlbumin(number);
+  }
   const history = useHistory();
   const navigateToHome = () => {
     const path = '/dashboard';
@@ -42,12 +68,40 @@ const AcidTest = () => {
           alignItems: 'center',
           flexDirection: 'column'
         }}>
-          <AcidTestInputBox label=''/>
-          <AcidTestInputBox label=''/>
-          <AcidTestInputBox label=''/>
-          <AcidTestInputBox label=''/>
-          <AcidTestInputBox label=''/>
-          <AcidTestInputBox label=''/>
+          <h4 className="ui header" style={{
+            color: "rgba(7,126,157,255)",
+            position: "relative",
+            bottom: '100px',
+            marginLeft: '20px'
+          }}>Laboratory Data</h4>
+          <h6 classname="ui header small" style={{
+            color: 'rgba(130,130,130,255',
+            position: 'relative',
+            bottom: '130px',
+            marginLeft: '20px',
+            size: '30px'
+          }}>
+            Please fill in the required fields below
+          </h6>
+          <div className="ui container" style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+            marginTop: '-150px'
+          }}>
+          <AcidTestInputBox callback={onPhChange} label1='pH' label2='' subscript='Normal Range 7.38-7.44'/>
+          <AcidTestInputBox callback={onHCChange} label1='HC03+' label2='mEq/L' subscript='Normal Range 23-28 mEq/L'/>
+          <AcidTestInputBox callback={onPCChange} label1='pC02' label2='mmHg' subscript='Normal Range 38-42 mmHg'/>
+          <AcidTestInputBox callback={onNaChange} label1='Na' label2='mEq/L' subscript='Normal Range 126-145 mEq/L'/>
+          <AcidTestInputBox callback={onClChange} label1='Cl' label2='mEq/L' subscript='Normal Range 98-106 mEq/L'/>
+          <AcidTestInputBox callback={onAlbuminChange} label1='Albumin' label2='mEq/L' subscript='Normal Range 3.5 - 5.5 meq/L'/>
+          </div>
+          <button className="ui button" style={{
+            color: 'white',
+            backgroundColor: 'rgba(7,126,157,255)',
+            marginTop: '40px'
+          }}>Calculate Results</button>
         </div>
       </div>
       <div style={{
---
+++ src/pages/AcidTestInputBox.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+
+
+const AcidTestInputBox = ({ callback, label1, label2, subscript }) => {
+  const [value, setValue] = useState("");
+  const handleChange = (e) => {
+    setValue(e.target.value);
+    callback(e.target.value);
+  }
+  return (
+    <div style={{ display: "flex", flexDirection: "column"}}>
+      <div style={{ display: "flex", alignItems: "center", marginTop: '15px' }}>
+        <span style={{ color: "rgba(7,126,157,255)", fontSize: "14px", minWidth: '58px', textAlign: 'right'}}>
+        {label1}&nbsp;
+        </span>
+        <div style={{ flexGrow: 1 }} />
+        <input
+          type="number"
+          step="0.01"
+          value={value}
+          onChange={handleChange}
+          style={{
+            width: "120px",
+            height: "30px",
+            padding: "0px 10px",
+            borderRadius: "5px",
+          }}
+        />
+        <div style={{ flexGrow: 1 }} />
+        <span style={{ color: "rgba(7,126,157,255)", fontSize: "14px", minWidth: '50px' }}>
+        &nbsp;{label2}
+        </span>
+      </div>
+      <div style={{ verticalAlign: 'top', fontSize: "10px", color: "rgba(130,130,130,255)", paddingTop: "5px", paddingLeft: '55px'}}>
+        {subscript}
+      </div>
+    </div>
+  );
+};
+
+
+
+export default AcidTestInputBox;
