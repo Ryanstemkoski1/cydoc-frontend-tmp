e7d5903d933f265e7bdad6c6fa7a9e25e9b1b536
vince21 <vincewils11@gmail.com>
2023-03-03 10:06:12 -0800

    rename hpiChiefComplaints.ts to favoriteChiefComplaints.ts

---
+++ src/constants/favoriteChiefComplaints.ts
@@ -0,0 +1,11 @@
+export const hpiChiefComplaints = [
+    'Hypertension',
+    'Diabetes',
+    'Urinary Symptoms/UTI',
+    'Upper Respiratory Infection',
+    'Depression',
+    'Insomnia',
+    'Hyperlipidemia',
+    'Opiate Use',
+    'Genital Symptoms/STI',
+];
