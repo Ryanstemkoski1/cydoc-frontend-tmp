93fa5096759ef9f845dbfe1936128324254a346c
SAKI HAYASHI <sh3467@nyu.edu>
2023-05-20 13:47:36 -0400

    src/pages/EditNote/content/physicalexam/InputSelectableTemparature.tsx

---
+++ src/pages/EditNote/content/physicalexam/InputSelectableTemparature.tsx
@@ -0,0 +1,83 @@
+import React, { SyntheticEvent, useState } from 'react';
+import {
+    Dropdown,
+    Input,
+    InputOnChangeData,
+    DropdownProps,
+} from 'semantic-ui-react';
+
+const options = [
+    { key: '°C', text: '°C', value: '°C' },
+    { key: '°F', text: '°F', value: '°F' },
+];
+
+const styledInput = {
+    width: 100,
+    height: 39,
+};
+const styledDropDown = {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+};
+interface Props {
+    handleChange: (val: string, data: InputOnChangeData) => void;
+}
+
+const InputSelectableTemparature = (props: Props) => {
+    const [currentUnit, setCurrentUnit] = useState<string>('°C');
+    const [currentTemparature, setCurrentTemparature] = useState<string>('0');
+    const celciusToForeignheight = () => {
+        return Number(currentTemparature) * 1.8 + 32;
+    };
+    const foreignheightToCelcius = () => {
+        return (Number(currentTemparature) - 32) * 0.5556;
+    };
+    const handleChangeInput = (
+        e: React.ChangeEvent<HTMLInputElement>,
+        data: InputOnChangeData
+    ) => {
+        setCurrentTemparature(data.value);
+        if (currentUnit === '°C') {
+            props.handleChange(currentTemparature, data);
+        } else {
+            props.handleChange(foreignheightToCelcius + '', data);
+        }
+    };
+    const toggleUnit = (
+        e: SyntheticEvent<HTMLElement, Event>,
+        data: DropdownProps
+    ) => {
+        if (currentUnit !== data.value) {
+            setCurrentUnit(data.value as string);
+            if (data.value === '°C') {
+                const val = foreignheightToCelcius();
+                setCurrentTemparature(Math.round(val) + '');
+            } else {
+                const val = celciusToForeignheight();
+                setCurrentTemparature(Math.round(val) + '');
+            }
+        }
+    };
+    return (
+        <Input
+            label={
+                <Dropdown
+                    defaultValue='°C'
+                    options={options}
+                    style={styledDropDown}
+                    onChange={toggleUnit}
+                />
+            }
+            labelPosition='right'
+            placeholder='0'
+            type='number'
+            style={styledInput}
+            className='selectable-temparature'
+            onChange={handleChangeInput}
+            name='temperature'
+            value={currentTemparature}
+        />
+    );
+};
+
+export default InputSelectableTemparature;
