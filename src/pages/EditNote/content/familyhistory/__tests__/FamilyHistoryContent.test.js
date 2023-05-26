73e79c37970d18c44c4131eaa11eac59463d7de6
SAKI HAYASHI <sh3467@nyu.edu>
2023-05-25 20:34:54 -0400

    cleaned lint err

--- src/pages/EditNote/content/familyhistory/__tests__/FamilyHistoryContent.test.js
+++ src/pages/EditNote/content/familyhistory/__tests__/FamilyHistoryContent.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Enzyme, { mount, shallow } from 'enzyme';
+import Enzyme, { mount } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
