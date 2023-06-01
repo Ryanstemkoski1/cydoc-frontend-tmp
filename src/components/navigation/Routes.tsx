9247dae423e9397233b5658432c3fa3a40b36086
Luke Weisensee <luke@synergetx.io>
2023-06-01 16:40:45 -0700

    updates dependencies, removes unused dependencies,

---
+++ src/components/navigation/Routes.tsx
@@ -0,0 +1,108 @@
+import Policy from 'constants/Documents/policy';
+import Terms_and_conditions from 'constants/Documents/terms_and_conditions';
+import LegacySignUpModal from 'pages/Account-Legacy/LegacySignUpModal';
+import EditProfile from 'pages/Account/EditProfile';
+import ForgotPasswordEmail from 'pages/Account/ForgotPasswordEmail';
+import Login from 'pages/Account/Login';
+import ProfileSecurity from 'pages/Account/ProfileSecurity';
+import AcidTest from 'pages/AcidTest';
+import CreateGraph from 'pages/CreateTemplate/CreateGraph';
+import EditGraph from 'pages/CreateTemplate/EditGraph';
+import EditTemplate from 'pages/CreateTemplate/EditTemplate';
+import EditNote from 'pages/EditNote/EditNote';
+import GenerateInpatientPlan from 'pages/GenerateInpatientPlan/GenerateInpatientPlan';
+import Home from 'pages/Home/Home';
+import LandingPage from 'pages/LandingPage/LandingPage';
+import LandingPagePublic from 'pages/LandingPage/LandingPagePublic';
+import ManagerDashboard from 'pages/ManagerDashboard/ManagerDashboard';
+import React from 'react';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import { PrivateRoute } from './PrivateRoute';
+
+const Routes = (props: { children?: JSX.Element | null }) => {
+    return (
+        <div>
+            <BrowserRouter>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/login' component={Login} />
+                <Route
+                    exact
+                    path='/hidden-users'
+                    component={LegacySignUpModal}
+                />
+                <Route
+                    exact
+                    path='/forgotpasswordemail'
+                    component={ForgotPasswordEmail}
+                />
+                <PrivateRoute exact path='/editnote' component={EditNote} />
+                <PrivateRoute
+                    exact
+                    path='/secretdashboard'
+                    component={LandingPage}
+                />
+                <PrivateRoute
+                    exact
+                    path='/dashboard'
+                    component={LandingPagePublic}
+                />
+                <PrivateRoute
+                    exact
+                    path='/generateinpatientplan'
+                    component={GenerateInpatientPlan}
+                />
+                <PrivateRoute exact path='/acid-test' component={AcidTest} />
+                <PrivateRoute
+                    exact
+                    path='/templates/new'
+                    component={CreateGraph}
+                />
+                <PrivateRoute
+                    exact
+                    path='/templates/old'
+                    component={EditGraph}
+                />
+                <PrivateRoute
+                    exact
+                    path='/templates/edit'
+                    component={EditTemplate}
+                />
+                <PrivateRoute
+                    exact
+                    path='/editprofile'
+                    component={EditProfile}
+                />
+                <PrivateRoute
+                    exact
+                    path='/profilesecurity'
+                    component={ProfileSecurity}
+                />
+                <PrivateRoute
+                    exact
+                    path='/managerdashboard'
+                    component={ManagerDashboard}
+                />
+                <Route exact path='/privacypolicy' component={Policy} />
+                <Route
+                    exact
+                    path='/termsandconditions'
+                    component={Terms_and_conditions}
+                />
+                <div className='footer-copyright'>
+                    Copyright © 2023 Cydoc Corporation. All rights reserved.
+                    Patent pending. &emsp;•&emsp;
+                    <a href='/privacypolicy' style={{ color: '#147A9B' }}>
+                        Privacy Policy
+                    </a>
+                    &emsp; • &emsp;
+                    <a href='/termsandconditions' style={{ color: '#147A9B' }}>
+                        Terms and Conditions
+                    </a>
+                </div>
+                {props.children}
+            </BrowserRouter>
+        </div>
+    );
+};
+export default Routes;
--- src/index.tsx
+++ src/index.tsx
@@ -2,37 +2,22 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { CookiesProvider } from 'react-cookie';
 import './semantic/dist/semantic.min.css';
-import { Route, BrowserRouter } from 'react-router-dom';
-import EditNote from './pages/EditNote/EditNote';
-import Login from './pages/Account/Login';
-import ForgotPasswordEmail from './pages/Account/ForgotPasswordEmail';
-import LandingPage from './pages/LandingPage/LandingPage';
-import LandingPagePublic from 'pages/LandingPage/LandingPagePublic';
-import Home from './pages/Home/Home';
-import { PrivateRoute } from './components/navigation/PrivateRoute';
-import CreateGraph from './pages/CreateTemplate/CreateGraph';
-import EditGraph from './pages/CreateTemplate/EditGraph';
-import EditTemplate from './pages/CreateTemplate/EditTemplate';
-import ManagerDashboard from './pages/ManagerDashboard/ManagerDashboard';
-import GenerateInpatientPlan from 'pages/GenerateInpatientPlan/GenerateInpatientPlan';
 import { HPIStore } from './contexts/HPIContext';
 import { AuthStore } from './contexts/AuthContext';
 import { NotesStore } from './contexts/NotesContext';
 import { HPITemplateStore } from './contexts/HPITemplateContext';
-import EditProfile from './pages/Account/EditProfile';
-import ProfileSecurity from './pages/Account/ProfileSecurity';
 import './index.css';
 import { Provider } from 'react-redux';
 import { currentNoteStore } from './redux/store';
-import Policy from './constants/Documents/policy';
-import Terms_and_conditions from './constants/Documents/terms_and_conditions';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import { isLivemode } from './auth/livemode';
-import AcidTest from 'pages/AcidTest';
+import Routes from 'components/navigation/Routes';
 
 const container = document.getElementById('root');
-const root = createRoot(container);
+// react's container is never null
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!);
 
 const getStripePublishableKey = () => {
     if (isLivemode()) {
@@ -44,122 +29,22 @@ const getStripePublishableKey = () => {
 
 const stripePromise = loadStripe(getStripePublishableKey());
 
-root.render(
-    <CookiesProvider>
-        <AuthStore>
-            <NotesStore>
-                <HPIStore>
-                    <HPITemplateStore>
-                        <Elements stripe={stripePromise}>
-                            <Provider store={currentNoteStore}>
-                                <div>
-                                    <BrowserRouter>
-                                        <Route
-                                            exact
-                                            path='/'
-                                            component={Home}
-                                        />
-                                        <Route
-                                            exact
-                                            path='/login'
-                                            component={Login}
-                                        />
-                                        <Route
-                                            exact
-                                            path='/forgotpasswordemail'
-                                            component={ForgotPasswordEmail}
-                                        />
-                                        <PrivateRoute
-                                            exact
-                                            path='/editnote'
-                                            component={EditNote}
-                                        />
-                                        <PrivateRoute
-                                            exact
-                                            path='/secretdashboard'
-                                            component={LandingPage}
-                                        />
-                                        <PrivateRoute
-                                            exact
-                                            path='/dashboard'
-                                            component={LandingPagePublic}
-                                        />
-                                        <PrivateRoute
-                                            exact
-                                            path='/generateinpatientplan'
-                                            component={GenerateInpatientPlan}
-                                        />
-                                        <PrivateRoute
-                                            exact
-                                            path='/acid-test'
-                                            component={AcidTest}
-                                        />
-                                        <PrivateRoute
-                                            exact
-                                            path='/templates/new'
-                                            component={CreateGraph}
-                                        />
-                                        <PrivateRoute
-                                            exact
-                                            path='/templates/old'
-                                            component={EditGraph}
-                                        />
-                                        <PrivateRoute
-                                            exact
-                                            path='/templates/edit'
-                                            component={EditTemplate}
-                                        />
-                                        <PrivateRoute
-                                            exact
-                                            path='/editprofile'
-                                            component={EditProfile}
-                                        />
-                                        <PrivateRoute
-                                            exact
-                                            path='/profilesecurity'
-                                            component={ProfileSecurity}
-                                        />
-                                        <PrivateRoute
-                                            exact
-                                            path='/managerdashboard'
-                                            component={ManagerDashboard}
-                                        />
-                                        <Route
-                                            exact
-                                            path='/privacypolicy'
-                                            component={Policy}
-                                        />
-                                        <Route
-                                            exact
-                                            path='/termsandconditions'
-                                            component={Terms_and_conditions}
-                                        />
-                                        <div className='footer-copyright'>
-                                            Copyright © 2023 Cydoc Corporation.
-                                            All rights reserved. Patent pending.
-                                            &emsp;•&emsp;
-                                            <a
-                                                href='/privacypolicy'
-                                                style={{ color: '#147A9B' }}
-                                            >
-                                                Privacy Policy
-                                            </a>
-                                            &emsp; • &emsp;
-                                            <a
-                                                href='/termsandconditions'
-                                                style={{ color: '#147A9B' }}
-                                            >
-                                                Terms and Conditions
-                                            </a>
-                                        </div>
-                                    </BrowserRouter>
-                                </div>
-                            </Provider>
-                        </Elements>
-                    </HPITemplateStore>
-                </HPIStore>
-            </NotesStore>
-        </AuthStore>
-    </CookiesProvider>,
-    container
-);
+function App() {
+    return (
+        <CookiesProvider>
+            <AuthStore>
+                <NotesStore>
+                    <HPIStore>
+                        <HPITemplateStore>
+                            <Elements stripe={stripePromise}>
+                                <Provider store={currentNoteStore}>
+                                    <Routes />
+                                </Provider>
+                            </Elements>
+                        </HPITemplateStore>
+                    </HPIStore>
+                </NotesStore>
+            </AuthStore>
+        </CookiesProvider>
+    );
+}
