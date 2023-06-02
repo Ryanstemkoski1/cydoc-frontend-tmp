f20cf9b91224a57b8f539ceb939f01204672aff3
Luke Weisensee <luke@synergetx.io>
2023-06-02 11:48:43 -0700

    preserve legacy sign-up logic under /hidden-signup in case we want it going forward

---
+++ src/pages/Account-Legacy/SignUpModal.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { Button, Modal } from 'semantic-ui-react';
+import './Account.css';
+import DoctorSignUp from './DoctorSignUp';
+
+interface SignupModalProps {
+    navToSignUp: boolean;
+    reloadModal: () => void;
+}
+
+const SignUpModal = (props: SignupModalProps) => {
+    const [isActive, setActive] = useState(props.navToSignUp);
+    const [continueActive, setContinueActive] = useState(true);
+
+    return (
+        <>
+            {continueActive ? (
+                <DoctorSignUp
+                    continueIsActive={continueActive}
+                    reloadModal={props.reloadModal}
+                />
+            ) : (
+                <Modal
+                    dimmer='inverted'
+                    size='tiny'
+                    onClose={() => {
+                        setActive(false);
+                        setContinueActive(false);
+                        props.reloadModal();
+                    }}
+                    onOpen={() => {
+                        setActive(true);
+                        setContinueActive(false);
+                    }}
+                    open={isActive}
+                >
+                    <Modal.Content>
+                        <div id='signup-modal-div'>
+                            <h3 id='signup-modal-title'>
+                                Sign up for a free 30-day trial
+                            </h3>
+                            <p id='signup-modal-text'>
+                                Write better notes, faster.
+                            </p>
+                            <p>
+                                First month $0, then $9/month. Cancel any time.
+                            </p>
+                        </div>
+                        <div id='signup-modal-btns'>
+                            <Button
+                                basic
+                                color='teal'
+                                content='Cancel'
+                                type='button'
+                                onClick={() => {
+                                    setActive(false);
+                                    props.reloadModal();
+                                    setContinueActive(false);
+                                }}
+                            />
+                            <Button
+                                color='teal'
+                                content='Continue'
+                                type='button'
+                                onClick={() => {
+                                    setActive(false);
+                                    setContinueActive(true);
+                                }}
+                            />
+                        </div>
+                    </Modal.Content>
+                </Modal>
+            )}
+        </>
+    );
+};
+
+export default SignUpModal;
