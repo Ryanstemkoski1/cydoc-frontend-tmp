70f407389cd8127279ac4fe1711cca2196abf0ad
Howie Youngdahl <hosidyoungdahl@gmail.com>
2023-06-03 21:53:16 -0400

    capitalization + hyphens
    doesn't remove multiple instances of capitalize and doesn't remove hyphens and parentheses

--- src/pages/EditNote/content/generatenote/generateHpiText.tsx
+++ src/pages/EditNote/content/generatenote/generateHpiText.tsx
@@ -26,7 +26,25 @@ interface PatientDisplayName {
 }
 
 const END_OF_SENTENCE_PUNC = '.!?';
-
+// selectively Lowercases anything except for multiple capitalized letters in a row 
+const selectivelyLowercase = (str: string): string => {
+    return str
+        .split(' ')
+        .map(word => {
+            console.log(word);
+            if(["ANSWER", "NOTANSWER", "ANSWER.", "NOTANSWER."].includes(word)){
+                return word.toLowerCase();
+            }
+                else{
+            const uppercaseCount = Array.from(word).reduce(
+                (count, char) => count + (char === char.toUpperCase() ? 1 : 0),
+                0
+            );
+            return uppercaseCount > 1 ? word : word.toLowerCase();
+                }
+        })
+        .join(' ');
+};
 /**
  * Removes whitespace from beginning and end of a sentence. Lowercases.
  * Removes punctuation (except periods, commas, forward slashes, apostrophes,
@@ -35,13 +53,14 @@ const END_OF_SENTENCE_PUNC = '.!?';
 export const fullClean = (sentence: string): string => {
     sentence = sentence.toLowerCase();
 
-    // remove punctuations
-    sentence = sentence.replace(/[^\w\s.,:/']/g, '');
+    // remove punctuations except hyphens and parentheses
+    sentence = sentence.replace(/[^\w\s.,:/()-]/g, '');
 
     // condense multiple whitespace into single space
     sentence = sentence.split(/\s+/).join(' ');
 
-    // remove period from the end of sentence (not all periods to preserve
+    // remove capitalized letters selectively
+    sentence = selectivelyLowercase(sentence);
     // decimal measurements of lesion)
     return sentence.replace(/\.\s?$/, '').trim();
 };
