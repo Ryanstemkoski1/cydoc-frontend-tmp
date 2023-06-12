f7551838100f0505f067cbf68cfe0e5a667ce5f1
Howie Youngdahl <hosidyoungdahl@gmail.com>
2023-06-12 12:10:20 -0400

    updates test
    updates test to reflect new changes

--- src/pages/EditNote/content/generatenote/combinesentences.test.js
+++ src/pages/EditNote/content/generatenote/combinesentences.test.js
@@ -24,7 +24,7 @@ test('combine regular sentences 4', () => {
             'The PATIENT has a hangnail.',
             3
         )
-    ).toBe(' The patient has a cellphone and a hangnail.');
+    ).toBe(' The patient has a cellphone and hangnail.');
 });
 test('combine regular sentences 5', () => {
     expect(
