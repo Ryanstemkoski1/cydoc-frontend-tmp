24f47f9265068255a266a6e47109a9563fe89fba
Luke Weisensee <luke@synergetx.io>
2023-06-26 20:59:32 -0400

    improves semantic ignore script

--- src/semantic/ignore-semantic-changes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "ignoring changes to sentry build files"
-
-git update-index --skip-worktree src/semantic/dist/components/*.css
-git update-index --skip-worktree src/semantic/dist/*.css
-
-echo "sentry build files ignored"
