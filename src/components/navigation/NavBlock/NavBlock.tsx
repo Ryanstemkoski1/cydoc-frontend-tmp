5c094414523a568d46a7c40119a93e3a2e34dbae
Vivian Obiako <vivianobiako1@gmail.com>
2024-07-19 16:19:29 +0100

    update styling of product settings page with new design

--- src/components/navigation/NavBlock/NavBlock.tsx
+++ src/components/navigation/NavBlock/NavBlock.tsx
@@ -3,60 +3,27 @@
 import style from './NavBlock.module.scss';
 import { Box, Typography } from '@mui/material';
 import { usePathname } from 'next/navigation';
-
-type RouteItem = {
-    path: string;
-    label: string;
-};
-
-const Routes: RouteItem[] = [
-    {
-        path: '/',
-        label: 'Home',
-    },
-    {
-        path: '/hpi/doctor',
-        label: 'Generated Notes',
-    },
-    {
-        path: '/qrcode',
-        label: 'Clinic QR Code & Link',
-    },
-    {
-        path: '/form-preferences',
-        label: 'Product Settings',
-    },
-    {
-        path: '/appointment-templates',
-        label: 'Appointment Templates',
-    },
-    {
-        path: '/manager-dashboard',
-        label: 'Manage Users',
-    },
-    {
-        path: '/subscription',
-        label: 'Subscription',
-    },
-    {
-        path: '/editprofile',
-        label: 'Edit Name or Phone',
-    },
-    {
-        path: '/profilesecurity',
-        label: 'Change Password',
-    },
-];
+import {
+    accountMenuItems,
+    clinicalWorkflowItems,
+    practiceAdminMenuItems,
+} from '@constants/drawerMenuItems';
 
 const NavBlock = () => {
     const pathname = usePathname();
 
-    const currentRoute = Routes.find((route) => route.path.includes(pathname));
+    const menuItems = [
+        ...clinicalWorkflowItems,
+        ...practiceAdminMenuItems,
+        ...accountMenuItems,
+    ];
+
+    const currentRoute = menuItems.find((item) => item.href.includes(pathname));
 
     return (
         <Box className={style.wrapper}>
             <Typography className={style.typo}>
-                {currentRoute?.label ?? 'Home'}
+                {pathname === '/' ? 'Home' : currentRoute?.label}
             </Typography>
         </Box>
     );
