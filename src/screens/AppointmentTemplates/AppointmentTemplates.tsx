846dd8758b5ff9cfeac331b39139880772818762
Vivian Obiako <vivianobiako1@gmail.com>
2024-07-23 20:10:42 +0100

    add a new modal for view more info

--- src/screens/AppointmentTemplates/AppointmentTemplates.tsx
+++ src/screens/AppointmentTemplates/AppointmentTemplates.tsx
@@ -1,8 +1,12 @@
 import style from './AppointmentTemplates.module.scss';
+import { useState } from 'react';
+import { Box, Button, IconButton, Modal, Typography } from '@mui/material';
 import Link from 'next/link';
-import { Box, IconButton, Typography } from '@mui/material';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
+import BorderColorRoundedIcon from '@mui/icons-material/BorderColorRounded';
+import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
 
 const tempData = [
     {
@@ -46,6 +50,75 @@ const tempData = [
 ];
 
 const AppointmentTemplatePage = () => {
+    const [viewMoreOpen, setViewMoreOpen] = useState<number>(0);
+
+    const handleViewMoreOpen = (index: number) => {
+        setViewMoreOpen(index);
+    };
+
+    const handleViewMoreClose = () => {
+        setViewMoreOpen(0);
+    };
+
+    const ViewMoreModal = () => (
+        <Modal
+            open={!!viewMoreOpen}
+            onClose={handleViewMoreClose}
+            aria-labelledby='viewMore-modal-title'
+            aria-describedby='viewMore-modal-description'
+        >
+            <Box className={style.viewMoreModalWrapper}>
+                <Box className={style.viewMoreModalWrapper__header}>
+                    <Typography component='p'>
+                        {tempData[viewMoreOpen - 1]?.header}
+                    </Typography>
+                    <IconButton>
+                        <CloseRoundedIcon />
+                    </IconButton>
+                </Box>
+                <Box className={style.viewMoreModalWrapper__body}>
+                    {tempData[viewMoreOpen - 1]?.body.map((item, idx) => (
+                        <Box
+                            key={idx}
+                            className={style.viewMoreModalWrapper__body__item}
+                        >
+                            <Typography
+                                component='p'
+                                className={
+                                    style.viewMoreModalWrapper__body__item__title
+                                }
+                            >
+                                {item.title}
+                            </Typography>
+                            <Typography
+                                component='p'
+                                className={
+                                    style.viewMoreModalWrapper__body__item__detail
+                                }
+                            >
+                                {item.detail}
+                            </Typography>
+                        </Box>
+                    ))}
+                    <Box
+                        className={
+                            style.viewMoreModalWrapper__body__viewMoreModalBtn
+                        }
+                    >
+                        <Button variant='outlined'>
+                            <BorderColorRoundedIcon />
+                            <span>Edit</span>
+                        </Button>
+                        <Button variant='outlined'>
+                            <DeleteRoundedIcon />
+                            <span>Delete</span>
+                        </Button>
+                    </Box>
+                </Box>
+            </Box>
+        </Modal>
+    );
+
     return (
         <Box className={style.apptTempWrapper}>
             {tempData.map((temp, index) => (
@@ -81,7 +154,10 @@ const AppointmentTemplatePage = () => {
                             </Box>
                         ))}
                         {temp.body.length > 3 && (
-                            <Box className={style.apptTempCard__body__viewMore}>
+                            <Box
+                                className={style.apptTempCard__body__viewMore}
+                                onClick={() => handleViewMoreOpen(index + 1)}
+                            >
                                 <Link href=''>View all information</Link>
                                 <ArrowForwardIosRoundedIcon />
                             </Box>
@@ -95,6 +171,7 @@ const AppointmentTemplatePage = () => {
                 </Box>
                 <Typography component='p'>Create a new template</Typography>
             </Box>
+            <ViewMoreModal />
         </Box>
     );
 };
